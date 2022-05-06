0028ce89d9e76d8ff330bb879613776ef145af39
n-rick <e.ficadiere@gmail.com>
2022-05-06 10:15:33 +0200

    cours: route avec fichier php

---
+++ config/old.routes.php
@@ -0,0 +1,9 @@
+<?php
+
+// use App\Controller\HomeController;
+// use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+// return function (RoutingConfigurator $routes) {
+//     $routes->add('home', '/home')
+//         ->controller([HomeController::class, 'index']);
+// };
