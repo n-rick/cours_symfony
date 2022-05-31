4745c242681dd8325af189947506f6713b68b1a1
n-rick <e.ficadiere@gmail.com>
2022-05-31 15:50:41 +0200

    chore: 31|05 cours => create database and migration

--- src/Entity/Personne.php
+++ src/Entity/Personne.php
@@ -2,33 +2,64 @@
 
 namespace App\Entity;
 
+use App\Repository\PersonneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonneRepository::class)]
 class Personne
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
     private $id;
+
+    #[ORM\Column(type: 'string', length: 30, nullable: true)]
     private $nom;
+
+    #[ORM\Column(type: 'string', length: 30, nullable: true)]
     private $prenom;
+
+    #[ORM\Column(type: 'string', length: 1, nullable: true)]
+    private $sexe;
+
     public function getId(): ?int
     {
         return $this->id;
     }
-    public function setId(int $id): void
-    {
-        $this->id = $id;
-    }
+
     public function getNom(): ?string
     {
         return $this->nom;
     }
-    public function setNom(string $nom): void
+
+    public function setNom(?string $nom): self
     {
         $this->nom = $nom;
+
+        return $this;
     }
+
     public function getPrenom(): ?string
     {
         return $this->prenom;
     }
-    public function setPrenom(string $prenom): void
+
+    public function setPrenom(?string $prenom): self
     {
         $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(?string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
     }
 }
