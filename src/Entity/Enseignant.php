7df537437c076ed69e7ab881ddbf9912426fa150
n-rick <e.ficadiere@gmail.com>
2022-06-01 14:25:49 +0200

    chore: 01|06 cours => heritage

---
+++ src/Entity/Enseignant.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EnseignantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EnseignantRepository::class)]
+class Enseignant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer', nullable: true)]
+    private $salaire;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSalaire(): ?int
+    {
+        return $this->salaire;
+    }
+
+    public function setSalaire(?int $salaire): self
+    {
+        $this->salaire = $salaire;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EtudiantRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EtudiantRepository::class)]
+class Etudiant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 30, nullable: true)]
+    private $niveau;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNiveau(): ?string
+    {
+        return $this->niveau;
+    }
+
+    public function setNiveau(?string $niveau): self
+    {
+        $this->niveau = $niveau;
+
+        return $this;
+    }
+}
