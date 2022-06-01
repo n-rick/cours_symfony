e66ea3dfad39d58e1cf91c19eaa0d19c7c68ce2f
n-rick <e.ficadiere@gmail.com>
2022-06-01 09:04:45 +0200

    chore: 01|06 cours => CRUD (C)

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -3,21 +3,27 @@
 namespace App\Controller;
 
 use App\Entity\Personne;
+use App\Repository\PersonneRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class PersonneController extends AbstractController
 {
 
     #[Route('/personne/add', name: 'personne_add')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
     {
         $personne = new Personne();
         $personne->setNom('Maradonna');
         $personne->setPrenom('Diego');
+        $errors = $validator->validate($personne);
+        if (count($errors) > 0) {
+            return new Response((string) $errors, 400);
+        }
         $entityManager->persist($personne);
         $entityManager->flush();
         return $this->render('personne/index.html.twig', [
@@ -26,4 +32,7 @@ class PersonneController extends AbstractController
             'adjectif' => 'ajoutée'
         ]);
     }
+
+
+
 }
