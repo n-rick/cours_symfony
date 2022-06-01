edc2cdbffa76c66f3b1415243a06af7d6b4abb89
n-rick <e.ficadiere@gmail.com>
2022-06-01 09:13:48 +0200

    chore: 01|06 cours => CRUD (R) by name

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -14,6 +14,9 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 class PersonneController extends AbstractController
 {
 
+    /**
+     * Ajouter une personne
+     */
     #[Route('/personne/add', name: 'personne_add')]
     public function index(EntityManagerInterface $entityManager, ValidatorInterface $validator): Response
     {
@@ -33,9 +36,11 @@ class PersonneController extends AbstractController
         ]);
     }
 
-
+    /**
+     * Rechercher une personne par l'id
+    */
     #[Route('/personne/{id}', name: 'personne_show')]
-    public function showPersonne(int $id, PersonneRepository $personneRepository): Response
+    public function showPersonneById(int $id, PersonneRepository $personneRepository): Response
     {
         $personne = $personneRepository->find($id);
         if (!$personne) {
@@ -48,4 +53,27 @@ class PersonneController extends AbstractController
             'adjectif' => 'recherchée'
         ]);
     }
+
+    /**
+     * Effectuer une recherche par nom et prenom
+     */
+    #[Route('/personne/{nom}/{personne}', name: 'personne_show_one')]
+    public function showPersonneByNameAndLastName(string $nom, string $prenom, PersonneRepository $personneRepository): Response
+    {
+        $personne = $personneRepository->findOneBy([
+            "nom" => $nom,
+            "prenom" => $prenom
+        ]);
+
+        if (!$personne) {
+            throw $this->createNotFoundException("Personne non trouvée.");
+        }
+
+        return $this->render('personne/index.html.twig', [
+            'controller_name' => 'PersonneController',
+            'personne' => $personne,
+            'adjectif' => 'recherchée'
+        ]);
+    }
+
 }
