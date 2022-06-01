2c504e3ae42b20855fd61dc9d125f601773c5968
n-rick <e.ficadiere@gmail.com>
2022-06-01 17:11:11 +0200

    chore: 01|06 cours => automatic CRUD

---
+++ src/Controller/VehicleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Vehicule;
+use App\Form\VehiculeType;
+use App\Repository\VehiculeRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/vehicle')]
+class VehicleController extends AbstractController
+{
+    #[Route('/', name: 'app_vehicle_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $vehicules = $entityManager
+            ->getRepository(Vehicule::class)
+            ->findAll();
+
+        return $this->render('vehicle/index.html.twig', [
+            'vehicules' => $vehicules,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_vehicle_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $vehicule = new Vehicule();
+        $form = $this->createForm(VehiculeType::class, $vehicule);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($vehicule);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_vehicle_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('vehicle/new.html.twig', [
+            'vehicule' => $vehicule,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_vehicle_show', methods: ['GET'])]
+    public function show(Vehicule $vehicule): Response
+    {
+        return $this->render('vehicle/show.html.twig', [
+            'vehicule' => $vehicule,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_vehicle_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Vehicule $vehicule, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(VehiculeType::class, $vehicule);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_vehicle_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('vehicle/edit.html.twig', [
+            'vehicule' => $vehicule,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_vehicle_delete', methods: ['POST'])]
+    public function delete(Request $request, Vehicule $vehicule, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$vehicule->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($vehicule);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_vehicle_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Entity/Vehicule.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VehiculeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VehiculeRepository::class)]
+class Vehicule
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $marque;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $modele;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $categorie;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->marque;
+    }
+
+    public function setMarque(?string $marque): self
+    {
+        $this->marque = $marque;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(?string $modele): self
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+
+    public function getCategorie(): ?string
+    {
+        return $this->categorie;
+    }
+
+    public function setCategorie(?string $categorie): self
+    {
+        $this->categorie = $categorie;
+
+        return $this;
+    }
+}
